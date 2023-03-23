55dab9097cffc3585dcf9eafbdce38bbe7970529
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-23 09:22:22 +0100

    terminado metodo area

--- Rectangulo.py
+++ Rectangulo.py
@@ -1,4 +1,5 @@
 import Punto
+from Rectangulo import Rectangulo
 class Rectangulo:
     def __init__(self, P_inicial, P_final): 
         if P_inicial and P_final == None:
@@ -19,4 +20,6 @@ class Rectangulo:
         comp_y_fin = self.P_final.y
         print("La Altura del rectangulo forma por los puntos:({},{}) y ({},{})".format(self.P_inicial.x, self.P_inicial.y, self.P_final.x, self.P_final.y)) 
         return abs(comp_y_ini - comp_y_fin)
-    
+    def Area(self):
+        print("Area del rectangulo:")
+        return Rectangulo.base(self)*Rectangulo.altura(self)
