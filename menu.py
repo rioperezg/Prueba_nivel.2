da521ad74df35b60862f53762f567871a31fad42
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-23 09:44:30 +0100

    haciendo menu punto cuadrante y vector hechos

--- menu.py
+++ menu.py
@@ -1,5 +1,5 @@
-import Punto 
-import Rectangulo
+from Punto import Punto 
+from Rectangulo import Rectangulo
 
 def iniciar():
     print("===================================")
@@ -15,11 +15,24 @@ def iniciar():
     print("===================================")
     opcion = input(">")
     if opcion == "1":
-    
+       print("Creando Punto...\n")
+       x = input("Introduzca la coordenada x de su punto:")
+       y = input("Introduzca la coordenada y de su punto:")
+       P = Punto(x, y)
+       print ("Punto creado")
+       return P.__str__()
     elif opcion == "2":
-
+       print("Consultando Cuadrante...\n")
+       x = input("Introduzca la coordenada x de su punto:")
+       y = input("Introduzca la coordenada y de su punto:")
+       P = Punto(x, y)
+       return P.cuadrante()
     elif opcion == "3":
-
+       print("Creando vectores...\n")
+       x = input("Introduzca la coordenada x de su punto:")
+       y = input("Introduzca la coordenada y de su punto:")
+       P = Punto(x, y)
+       return P.vector()
     elif opcion == "4":        
 
     elif opcion == "5":
