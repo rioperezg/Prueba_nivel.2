f5a15e1111685c376e0c72c5b9e058f2be98c056
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-23 08:51:53 +0100

    haceindo metodo cuadrante

--- Punto.py
+++ Punto.py
@@ -6,4 +6,15 @@ class Punto:
             self.x = 0
             self.y = 0
     def __str__(self):
-        return "Punto:({},{})".format(self.x, self.y)        
+        return "Punto:({},{})".format(self.x, self.y)   
+    def cuadrante(self):
+        if self.x > 0:
+            if self.y > 0:
+                return "El punto pertenece al primer cuadrante"
+            elif self.y < 0:
+                return "El punto pertenece al cuarto cuadrante"
+            else:
+                return "El punto se encuentra sobre el eje x"
+        elif self.x < 0:
+            if self.y > 0:
+                return "El punto pertenece al "       
