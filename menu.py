1fd44c10796db51f80292c248113af5f3110d147
rioperezg <113980673+rioperezg@users.noreply.github.com>
2023-03-23 09:35:21 +0100

    haciendo menu

---
+++ menu.py
@@ -0,0 +1,33 @@
+import Punto 
+import Rectangulo
+
+def iniciar():
+    print("===================================")
+    print("===================================")
+    print("[1] Crear Puntos                   ")
+    print("[2] Consultar Cuadrante            ")
+    print("[3] Crear Vectores                 ")
+    print("[4] Consultar distancia            ")
+    print("[5] Crear Rectangulos              ")
+    print("[6] Consultar base del Rectangulo  ")
+    print("[7] Consultar Altura del Rectangulo")
+    print("[8] Consultar Area del Rectangulo  ")
+    print("===================================")
+    opcion = input(">")
+    if opcion == "1":
+    
+    elif opcion == "2":
+
+    elif opcion == "3":
+
+    elif opcion == "4":        
+
+    elif opcion == "5":
+
+    elif opcion == "6":
+
+    elif opcion == "7":
+
+
+
+
--- run.py
+++ run.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "-t":
         menu.iniciar()
     else:
-        app = ui.MainWindow()
-        app.mainloop()
+        # app = ui.MainWindow()
+        # app.mainloop()
+        pass
